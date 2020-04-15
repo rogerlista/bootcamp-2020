b9e78537257fd0a0adcc679206d3f3001c8bf969
Rogério <roger.lista@gmail.com>
2020-04-15 06:52:47 -0300

    Aplicação funcional - CRUD projetos

--- 01-conceitos-dev/backend/src/index.js
+++ 01-conceitos-dev/backend/src/index.js
@@ -1,41 +1,61 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
+const projects = []
 
 app.use(express.json())
 
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query
+  const { title } = request.query
 
-  console.log(title)
-  console.log(owner)
+  const results = title ? projects.filter((project) => project.title.includes(title)) : projects
 
-  return response.json(['Project 1', 'Project 2'])
+  return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body
+  const project = { id: uuid(), title, owner }
 
-  console.log(title)
-  console.log(owner)
+  projects.push(project)
 
-  return response.json(['Project 1', 'Project 2', 'Project3'])
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params
+  const { title, owner } = request.body
 
-  console.log(id)
+  const projectIndex = projects.findIndex((project) => project.id === id)
 
-  return response.json(['Project 4', 'Project 2', 'Project 3'])
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
   const { id } = request.params
 
-  console.log(id)
+  const projectIndex = projects.findIndex((project) => project.id === id)
 
-  return response.json(['Project 2', 'Project 3'])
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(3333, () => console.log('🚀 Back-end started!'))
