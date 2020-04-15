125b61e7864f9d0d2f878208a4b0858ceaaedbf7
Rogério <roger.lista@gmail.com>
2020-04-15 05:33:15 -0300

    Configurado Nodemon

--- 01-conceitos-dev/backend/src/index.js
+++ 01-conceitos-dev/backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' })
+  return response.json({ message: 'Hello GoStack!' })
 })
 
 app.listen(3333, () => console.log('🚀 Back-end started!'))
