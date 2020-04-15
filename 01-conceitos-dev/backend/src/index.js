e9a8f7090389080428193f2b8936fdad203e6fad
Rogério <roger.lista@gmail.com>
2020-04-15 07:06:59 -0300

    Configurado Cors

--- 01-conceitos-dev/backend/src/index.js
+++ 01-conceitos-dev/backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
 const projects = []
 
+app.use(cors())
 app.use(express.json())
 
 function logRequests(request, response, next) {
