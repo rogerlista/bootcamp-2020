16282219a7586a8d6a2ba24cf59d30b735f3d594
Rogério <roger.lista@gmail.com>
2020-04-15 06:13:54 -0300

    Métodos HTTP

--- 01-conceitos-dev/backend/src/index.js
+++ 01-conceitos-dev/backend/src/index.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!' })
+app.get('/projects', (request, response) => {
+  return response.json(['Project 1', 'Project 2'])
+})
+
+app.post('/projects', (request, response) => {
+  return response.json(['Project 1', 'Project 2', 'Project3'])
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json(['Project 4', 'Project 2', 'Project 3'])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json(['Project 2', 'Project 3'])
 })
 
 app.listen(3333, () => console.log('🚀 Back-end started!'))
