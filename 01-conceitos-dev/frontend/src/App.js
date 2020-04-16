d593c9829d668b4986c2c31b860a499a71f0f08f
Rogério <roger.lista@gmail.com>
2020-04-16 17:05:11 -0300

    Iniciando manualmente uma aplicação ReactJS - estado e imutabilidade com useState

--- 01-conceitos-dev/frontend/src/App.js
+++ 01-conceitos-dev/frontend/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 
 export default function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`])
+    console.log(projects)
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
+      <Header title="Homepage" />
 
-      <Header title="Projects">
+      <ul>
         <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
+          {projects.map((project) => (
+            <li key={project}>{project}</li>
+          ))}
         </ul>
-      </Header>
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Projeto
+      </button>
     </>
   )
 }
