9886bded6f6d59413ee70baf07a3b1e66b07d04e
Rogério <roger.lista@gmail.com>
2020-04-16 16:50:03 -0300

    Iniciando manualmente uma aplicação ReactJS - trabalhando com propriedades

--- 01-conceitos-dev/frontend/src/App.js
+++ 01-conceitos-dev/frontend/src/App.js
@@ -4,8 +4,20 @@ import Header from './components/Header'
 export default function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   )
 }
