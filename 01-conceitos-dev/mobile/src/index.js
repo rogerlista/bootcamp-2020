6e0fd66e93d35056a066dc53b8792ef2038c5a9b
Rogério <roger.lista@gmail.com>
2020-04-17 16:30:17 -0300

    Iniciando uma aplicação React Native - diferenças no elemento que são componentes e na estilização

--- 01-conceitos-dev/mobile/src/index.js
+++ 01-conceitos-dev/mobile/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StatusBar, StyleSheet } from 'react-native'
 
 export default function App() {
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle="light-content" background="#7159c1" />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello Gostack</Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+})
