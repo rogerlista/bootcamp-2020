c0c893740507fcbaca5ba97cb49f0a759f77c9aa
Rogério <roger.lista@gmail.com>
2020-04-20 06:56:34 -0300

    Back-end - configurada a pasta routes

--- 02-primeiro-projeto-node/src/server.ts
+++ 02-primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+import routes from "./routes";
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!' })
-})
+const app = express();
+
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log("🚀 Server started on port 3333!");
+});
