ab6c2a8e7c76e7bcdfc4c10541d883e283363acf
Rogério <roger.lista@gmail.com>
2020-04-21 14:09:29 -0300

    Back-end - trabalhando dados com tipos

--- 02-primeiro-projeto-node/src/routes/appointments.routes.ts
+++ 02-primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -23,7 +23,10 @@ appointmentsRouter.post('/', (request, response) => {
     return response.status(400).json({ message: 'This appointment is already booked' })
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate)
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  })
 
   return response.json(appointment)
 })
