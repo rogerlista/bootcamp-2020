e24e4eb68cf0cdd0806b8373599bfa0fa614fc66
Rogério <roger.lista@gmail.com>
2020-04-21 13:29:59 -0300

    Back-end - criado o model de agendamento

--- 02-primeiro-projeto-node/src/routes/appointments.routes.ts
+++ 02-primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -2,6 +2,8 @@ import Router from 'express'
 import { uuid } from 'uuidv4'
 import { startOfHour, parseISO, isEqual } from 'date-fns'
 
+import Appointment from '../models/Appointment'
+
 const appointmentsRouter = Router()
 
 interface Appointment {
@@ -23,11 +25,7 @@ appointmentsRouter.post('/', (request, response) => {
     return response.status(400).json({ message: 'This appointment is already booked' })
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  }
+  const appointment = new Appointment(provider, parsedDate)
 
   appointments.push(appointment)
 
