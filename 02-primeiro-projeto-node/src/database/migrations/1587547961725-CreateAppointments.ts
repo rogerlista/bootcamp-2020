ae29a091261cfd6b1e9029c28c09d333dc682a9b
Rogério <roger.lista@gmail.com>
2020-04-23 06:31:16 -0300

    Back-end - correções da criação da tabela agendamento

--- 02-primeiro-projeto-node/src/database/migrations/1587547961725-CreateAppointments.ts
+++ 02-primeiro-projeto-node/src/database/migrations/1587547961725-CreateAppointments.ts
@@ -11,16 +11,25 @@ export default class CreateAppointments1587547961725 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
