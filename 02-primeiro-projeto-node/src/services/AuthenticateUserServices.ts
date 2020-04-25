adb1beb9e277283ab75bffcf788fffc1b0d9797a
Rogério <roger.lista@gmail.com>
2020-04-25 19:28:50 -0300

    Back-end - gerado token jwt

--- 02-primeiro-projeto-node/src/services/AuthenticateUserServices.ts
+++ 02-primeiro-projeto-node/src/services/AuthenticateUserServices.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
@@ -23,11 +25,16 @@ class AuthenticateUserService {
 
     const passwordMatched = await compare(password, user.password)
 
-    if (passwordMatched) {
+    if (!passwordMatched) {
       throw new Error('Incorrect email/password combination.')
     }
 
-    return { user }
+    const token = sign({}, 'b0c984f14569c7d395bc85106d334324', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    return { user, token }
   }
 }
 
