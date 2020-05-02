9c841d50c2d8cd02360e27ef40dacf9084194cea
Rogério <roger.lista@gmail.com>
2020-05-02 13:07:41 -0300

    Front-end - criado as rotas dashboard e repository

--- 03-primeiro-projeto-react/src/App.tsx
+++ 03-primeiro-projeto-react/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Router from './routes'
 
 const App: React.FC = () => {
-  return <h1>Github Explorer</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
 
 export default App
