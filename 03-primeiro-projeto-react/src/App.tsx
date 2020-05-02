640cd8065a9cace8c8e77768695162936c3c06cb
Rogério <roger.lista@gmail.com>
2020-05-02 14:00:52 -0300

    Front-end - configurado styled-components

--- 03-primeiro-projeto-react/src/App.tsx
+++ 03-primeiro-projeto-react/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import GlobalStyle from './styles/global'
+
 import Router from './routes'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   )
 }
 
