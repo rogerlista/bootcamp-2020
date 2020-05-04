b34f78f9981a87ac3d489ab32f48810b3e5e1586
Rogério <roger.lista@gmail.com>
2020-05-04 16:04:07 -0300

    Front-end - conectando a API

--- 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 03-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,72 +1,62 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
 
 import { Title, Form, Repositories } from './styles'
 
+interface Repository {
+  full_name: string
+  description: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault()
+
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+    setNewRepo('')
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios do Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4" alt="Rogério Gomes Rios" />
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
 
-          <div>
-            <strong>rogerlista/bootcamp-2020</strong>
-            <p>Bootcamp Rockectseat 2020</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4" alt="Rogério Gomes Rios" />
-
-          <div>
-            <strong>rogerlista/bootcamp-2020</strong>
-            <p>Bootcamp Rockectseat 2020</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4" alt="Rogério Gomes Rios" />
-
-          <div>
-            <strong>rogerlista/bootcamp-2020</strong>
-            <p>Bootcamp Rockectseat 2020</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4" alt="Rogério Gomes Rios" />
-
-          <div>
-            <strong>rogerlista/bootcamp-2020</strong>
-            <p>Bootcamp Rockectseat 2020</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4" alt="Rogério Gomes Rios" />
-
-          <div>
-            <strong>rogerlista/bootcamp-2020</strong>
-            <p>Bootcamp Rockectseat 2020</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   )
