c19f337ada6d7221be87745b4406e60ba21077f7
Rogério <roger.lista@gmail.com>
2020-05-09 18:51:10 -0300

    GoBarber web - validando cadastro de usuário

--- 04-gobarber-web/src/pages/SignUp/index.tsx
+++ 04-gobarber-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -10,9 +11,23 @@ import logoImg from '../../assets/logo.svg'
 import { Container, Background, Content } from './styles'
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data)
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
 
   return (
     <Container>
