c0fdb14cba02f244dd066cdd0c474da8be4ae2ff
Rogério <roger.lista@gmail.com>
2020-05-11 18:08:18 -0300

    GoBarber web - realizando o login pelo contexto

--- 04-gobarber-web/src/App.tsx
+++ 04-gobarber-web/src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 import GlobalStyle from './styles/global'
 
-import AuthContext from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext'
 
 import SignIn from './pages/SignIn'
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Juca' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
--- 04-gobarber-web/src/context/AuthContext.tsx
+++ 04-gobarber-web/src/context/AuthContext.tsx
@@ -1,9 +1,34 @@
-import { createContext } from 'react'
+import React, { createContext, useCallback } from 'react'
+
+import api from '../services/api'
+
+interface SignInCredentials {
+  email: string
+  password: string
+}
 
 interface AuthContextData {
   name: string
+  signIn(credentials: SignInCredentials): Promise<void>
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-export default AuthContext
+const AuthProvider: React.FC = ({ children }) => {
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    })
+
+    console.log(response.data)
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ name: 'Juca', signIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthProvider }
