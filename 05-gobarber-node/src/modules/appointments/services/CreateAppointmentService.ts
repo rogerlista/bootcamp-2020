7123bdfbb96e5303314ad2b8bf714355eb0b0c09
Rogério <roger.lista@gmail.com>
2020-05-26 20:30:45 -0300

    GoBarber node - dependency inversion principle

--- 05-gobarber-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ 05-gobarber-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
-import { getCustomRepository } from 'typeorm'
 import { startOfHour } from 'date-fns'
 
 import AppError from '@shared/errors/AppError'
+
 import Appointment from '../infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
 interface IRequest {
   provider_id: string
@@ -11,17 +11,18 @@ interface IRequest {
 }
 
 class CreateAppointmentService {
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
   public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate)
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate)
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked')
     }
 
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     })
