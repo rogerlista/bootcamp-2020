d22276f9c83e578adbeaecf1714bbb4cde8e57da
Rogério <roger.lista@gmail.com>
2020-05-26 20:23:05 -0300

    GoBarber node - reescrevendo repositorios

--- 05-gobarber-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ 05-gobarber-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -5,13 +5,13 @@ import AppError from '@shared/errors/AppError'
 import Appointment from '../infra/typeorm/entities/Appointment'
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
-interface Request {
+interface IRequest {
   provider_id: string
   date: Date
 }
 
 class CreateAppointmentService {
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
     const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
@@ -21,12 +21,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked')
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     })
-
-    await appointmentsRepository.save(appointment)
 
     return appointment
   }
