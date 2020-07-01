8304aa9caa629def818a3030084fdcaf83f3e25a
Rogério <roger.lista@gmail.com>
2020-07-01 16:40:34 -0300

    GoBarber node - token para a recuperação de senha

---
+++ 05-gobarber-node/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,27 @@
+import { uuid } from 'uuidv4'
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository'
+
+import UserToken from '../../infra/typeorm/entities/UserToken'
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = []
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken()
+
+    Object.assign(userToken, {
+      id: uuid(),
+      toke: uuid(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+
+    this.userTokens.push(userToken)
+
+    return userToken
+  }
+}
+
+export default FakeUserTokensRepository
