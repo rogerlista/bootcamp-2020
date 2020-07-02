ef98d1ef15bb9eb24396953c5aea35ea81a860cc
Rogério <roger.lista@gmail.com>
2020-07-02 13:36:07 -0300

    GoBarber node - salvando tokens no banco de dados

--- 05-gobarber-node/src/shared/infra/http/routes/index.ts
+++ 05-gobarber-node/src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes'
+import passwordRouter from '@modules/users/infra/http/routes/password.routes'
 import usersRouter from '@modules/users/infra/http/routes/users.routes'
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes'
 
 const routes = Router()
 
 routes.use('/sessions', sessionsRouter)
+routes.use('/password', passwordRouter)
 routes.use('/appointments', appointmentsRouter)
 routes.use('/users', usersRouter)
 
