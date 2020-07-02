5679490707debc941cdde52985f4511078585f0f
Rogério <roger.lista@gmail.com>
2020-07-02 17:50:47 -0300

    GoBarber node - envio de e-mail em desenvolvimento

--- 05-gobarber-node/src/shared/container/providers/index.ts
+++ 05-gobarber-node/src/shared/container/providers/index.ts
@@ -1,9 +1,11 @@
-import { container } from 'tsyringe';
+import { container } from 'tsyringe'
 
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IStorageProvider from './StorageProvider/models/IStorageProvider'
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
 
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
+import IMailProvider from './MailProvider/models/IMailProvider'
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
+
+container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider)
+
+container.registerInstance<IMailProvider>('MailProvider', new EtherealMailProvider())
