6b9af14c31b21615cb10cc53c72556d2c689653b
Rogério <roger.lista@gmail.com>
2020-07-03 19:40:58 -0300

    GoBarber node - rota e controller do perfil do usuário

--- 05-gobarber-node/src/shared/infra/http/routes/index.ts
+++ 05-gobarber-node/src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes'
 import passwordRouter from '@modules/users/infra/http/routes/password.routes'
+import profileRouter from '@modules/users/infra/http/routes/profile.routes'
 import usersRouter from '@modules/users/infra/http/routes/users.routes'
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes'
 
@@ -9,6 +10,7 @@ const routes = Router()
 
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
+routes.use('/profile', profileRouter)
 routes.use('/appointments', appointmentsRouter)
 routes.use('/users', usersRouter)
 
