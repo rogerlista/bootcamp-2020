20bb35b0acd890d9f8d7361f71db9e4d92a0094c
Rogério <roger.lista@gmail.com>
2020-07-04 14:06:27 -0300

    GoBarber node - listagem de prestadores

--- 05-gobarber-node/src/shared/infra/http/routes/index.ts
+++ 05-gobarber-node/src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import passwordRouter from '@modules/users/infra/http/routes/password.routes'
 import profileRouter from '@modules/users/infra/http/routes/profile.routes'
 import usersRouter from '@modules/users/infra/http/routes/users.routes'
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes'
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes'
 
 const routes = Router()
 
@@ -12,6 +13,7 @@ routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter)
 routes.use('/appointments', appointmentsRouter)
+routes.use('/providers', providersRouter)
 routes.use('/users', usersRouter)
 
 export default routes
