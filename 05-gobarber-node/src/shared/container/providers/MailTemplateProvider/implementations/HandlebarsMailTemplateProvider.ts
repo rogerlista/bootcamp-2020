8277c4e8be192d8f67c5f84094c25bd393695d94
Rogério <roger.lista@gmail.com>
2020-07-11 09:54:07 -0300

    GoBarber node - organizando o container

--- 05-gobarber-node/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ 05-gobarber-node/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -5,7 +5,10 @@ import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO'
 import IMailTemplateProvider from '../models/IMailTemplateProvider'
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ file, variables }: IParseMailTemplateDTO): Promise<string> {
+  public async parse({
+    file,
+    variables,
+  }: IParseMailTemplateDTO): Promise<string> {
     const templateFileContent = await fs.promises.readFile(file, {
       encoding: 'utf-8',
     })
