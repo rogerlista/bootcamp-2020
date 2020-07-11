63f6ac29de5eb7cc42dfc8230421d3d157105417
Rogério <roger.lista@gmail.com>
2020-07-11 11:20:44 -0300

    GoBarber node - upload de arquivos para o Amazon S3

--- 05-gobarber-node/src/modules/users/services/UpdateUserAvatarService.ts
+++ 05-gobarber-node/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,8 +1,5 @@
-import fs from 'fs'
-import path from 'path'
 import { injectable, inject } from 'tsyringe'
 
-import uploadConfig from '@config/upload'
 import AppError from '@shared/errors/AppError'
 
 import IUsersRepository from '../repositories/IUsersRepository'
