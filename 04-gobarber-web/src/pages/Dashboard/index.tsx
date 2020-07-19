a39d1425f5b52d2121117ea56c42f439323b6fd7
Rogério <roger.lista@gmail.com>
2020-07-19 12:34:42 -0300

    GoBarber web - dasboard próximo agendamento

--- 04-gobarber-web/src/pages/Dashboard/index.tsx
+++ 04-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiClock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 import { useAuth } from '../../hooks/auth'
 
 import logoImg from '../../assets/logo.svg'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles'
 
 const Dashboard: React.FC = () => {
   const { user, signOut } = useAuth()
@@ -31,6 +40,48 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Juca"
+              />
+
+              <strong>Juca</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Juca"
+              />
+
+              <strong>Juca</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+      </Content>
     </Container>
   )
 }
