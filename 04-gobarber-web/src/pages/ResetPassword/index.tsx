bac1bd3e5a37f0d20cb509be847eb28f0c2b6031
Rogério <roger.lista@gmail.com>
2020-07-19 06:47:52 -0300

    GoBarber web - criada a página de redefinição da senha do usuário

--- 04-gobarber-web/src/pages/ResetPassword/index.tsx
+++ 04-gobarber-web/src/pages/ResetPassword/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 import { FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
+
+import { useToast } from '../../hooks/toast'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -10,14 +14,59 @@ import { Container, Content, AnimationContainer, Background } from './styles'
 
 import logoImg from '../../assets/logo.svg'
 
+interface ResetPasswordFormData {
+  password: string
+  password_confirmation: string
+}
+
 const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+
+  const { addToast } = useToast()
+
+  const history = useHistory()
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({})
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Senha obrigatória'),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password'), null],
+            'Confirmação incorreta',
+          ),
+        })
+
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        history.push('/')
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          return
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao trocar a senha',
+          description:
+            'Ocorreu um erro ao tentar trocar sua senha, tente novamente.',
+        })
+      }
+    },
+    [history, addToast],
+  )
+
   return (
     <Container>
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
 
-          <Form onSubmit={() => {}}>
+          <Form onSubmit={handleSubmit}>
             <h1>Resetar senha</h1>
 
             <Input
