970cded308ce8ebb709b4b67014e59fc19492e3d
Rogério <roger.lista@gmail.com>
2020-07-19 12:12:17 -0300

    GoBarber web - criado o header do dasboard

--- 04-gobarber-web/src/pages/Dashboard/index.tsx
+++ 04-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import { FiPower } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
+import { useAuth } from '../../hooks/auth'
+
+import logoImg from '../../assets/logo.svg'
+
+import { Container, Header, HeaderContent, Profile } from './styles'
+
+const Dashboard: React.FC = () => {
+  const { user, signOut } = useAuth()
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  )
+}
 
 export default Dashboard
