050a2a7de68d7a22595e37d44d8b30e53b838773
Rogério <roger.lista@gmail.com>
2020-07-20 18:54:06 -0300

    GoBarber web - implentação das alterações dos dados do perfil do usuário

--- 04-gobarber-web/src/pages/Profile/index.tsx
+++ 04-gobarber-web/src/pages/Profile/index.tsx
@@ -21,7 +21,9 @@ import { Container, Content, AvatarInput } from './styles'
 interface ProfileFormData {
   name: string
   email: string
+  old_password: string
   password: string
+  password_confirmation: string
 }
 
 const Profile: React.FC = () => {
@@ -41,21 +43,60 @@ const Profile: React.FC = () => {
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: (val) => !!val.length,
+            then: Yup.string()
+              .min(6, 'No mínimo 6 dígitos')
+              .required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: (val) => !!val.length,
+              then: Yup.string()
+                .min(6, 'No mínimo 6 dígitos')
+                .required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password'), null, 'Confirmação incorreta']),
         })
 
         await schema.validate(data, {
           abortEarly: false,
         })
 
-        await api.post('/users', data)
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data
 
-        history.push('/')
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        }
+
+        const response = await api.put('/profile', formData)
+
+        updateUser(response.data)
+
+        history.push('/dashboard')
 
         addToast({
           type: 'success',
-          title: 'Cadastro realizado',
-          description: 'Você já pode fazer seu logon',
+          title: 'Perfil atualizado',
+          description:
+            'Suas informações do perfil foram atualizadas con sucesso!',
         })
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -68,12 +109,13 @@ const Profile: React.FC = () => {
 
         addToast({
           type: 'error',
-          title: 'Error no cadastro',
-          description: 'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+          title: 'Error na atualização',
+          description:
+            'Ocorreu um erro ao atualizar o perfil, tente novamente.',
         })
       }
     },
-    [addToast, history],
+    [addToast, history, updateUser],
   )
 
   const handleAvatarChange = useCallback(
@@ -133,12 +175,19 @@ const Profile: React.FC = () => {
             containerStyle={{ marginTop: 24 }}
             name="old_password"
             icon={FiLock}
+            type="password"
             placeholder="Senha atual"
           />
-          <Input name="password" icon={FiLock} placeholder="Nova senha" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Nova senha"
+          />
           <Input
             name="password_confirmation"
             icon={FiLock}
+            type="password"
             placeholder="Confirmar senha"
           />
 
