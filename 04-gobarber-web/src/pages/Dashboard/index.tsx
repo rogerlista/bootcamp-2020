62e6417ad71aa4b7f114e249bf7603a942b82af3
Rogério <roger.lista@gmail.com>
2020-07-20 07:22:29 -0300

    GoBarber web - obtendo agendamentos da API

--- 04-gobarber-web/src/pages/Dashboard/index.tsx
+++ 04-gobarber-web/src/pages/Dashboard/index.tsx
@@ -3,6 +3,8 @@ import { FiPower, FiClock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import DayPicker, { DayModifiers } from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
+import { format, isToday } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 import api from '../../services/api'
 
@@ -28,12 +30,22 @@ interface MonthAvailabilityItem {
   available: boolean
 }
 
+interface Appointment {
+  id: string
+  date: string
+  user: {
+    name: string
+    avatar_url: string
+  }
+}
+
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
   const [currentMonth, setCurrentMonth] = useState(new Date())
   const [monthAvailability, setMonthAvailability] = useState<
     MonthAvailabilityItem[]
   >([])
+  const [appointments, setAppointments] = useState<Appointment[]>([])
 
   const { user, signOut } = useAuth()
 
@@ -60,6 +72,20 @@ const Dashboard: React.FC = () => {
       })
   }, [currentMonth, user.id])
 
+  useEffect(() => {
+    api
+      .get('/appointment/me', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then((response) => {
+        setAppointments(response.data)
+      })
+  }, [selectedDate])
+
   const disabledDays = useMemo(() => {
     const dates = monthAvailability
       .filter((monthDay) => monthDay.available === false)
@@ -72,6 +98,18 @@ const Dashboard: React.FC = () => {
 
     return dates
   }, [currentMonth, monthAvailability])
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM", {
+      locale: ptBR,
+    })
+  }, [selectedDate])
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc', {
+      locale: ptBR,
+    })
+  }, [selectedDate])
 
   return (
     <Container>
@@ -100,9 +138,9 @@ const Dashboard: React.FC = () => {
           <h1>Horários agendados</h1>
 
           <p>
-            <span>Hoje</span>
-            <span>Dia 06</span>
-            <span>Segunda-feira</span>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{selectedWeekDay}</span>
           </p>
 
           <NextAppointment>
