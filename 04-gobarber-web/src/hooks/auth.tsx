ade7f4cac094939c235c1a5da8e2dc0f0a7a24a2
Rogério <roger.lista@gmail.com>
2020-07-20 06:59:17 -0300

    GoBarber web - dias disponiveis no mês

--- 04-gobarber-web/src/hooks/auth.tsx
+++ 04-gobarber-web/src/hooks/auth.tsx
@@ -5,6 +5,7 @@ import api from '../services/api'
 interface User {
   id: string
   name: string
+  email: string
   avatar_url: string
 }
 interface AuthState {
@@ -31,6 +32,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user')
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
       return { token, user: JSON.parse(user) }
     }
 
@@ -47,6 +50,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token)
     localStorage.setItem('@GoBarber:user', JSON.stringify(user))
+
+    api.defaults.headers.authorization = `Bearer ${token}`
 
     setData({ token, user })
   }, [])
--- 04-gobarber-web/src/pages/Dashboard/index.tsx
+++ 04-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect, useMemo } from 'react'
 import { FiPower, FiClock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import DayPicker, { DayModifiers } from 'react-day-picker'
 import 'react-day-picker/lib/style.css'
+
+import api from '../../services/api'
 
 import { useAuth } from '../../hooks/auth'
 
@@ -21,16 +23,56 @@ import {
   Calendar,
 } from './styles'
 
+interface MonthAvailabilityItem {
+  day: number
+  available: boolean
+}
+
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
+  const [currentMonth, setCurrentMonth] = useState(new Date())
+  const [monthAvailability, setMonthAvailability] = useState<
+    MonthAvailabilityItem[]
+  >([])
 
   const { user, signOut } = useAuth()
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available) {
+    if (modifiers.available && !modifiers.disabled) {
       setSelectedDate(day)
     }
   }, [])
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month)
+  }, [])
+
+  useEffect(() => {
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then((response) => {
+        setMonthAvailability(response.data)
+      })
+  }, [currentMonth, user.id])
+
+  const disabledDays = useMemo(() => {
+    const dates = monthAvailability
+      .filter((monthDay) => monthDay.available === false)
+      .map((monthDay) => {
+        const year = currentMonth.getFullYear()
+        const month = currentMonth.getMonth()
+
+        return new Date(year, month, monthDay.day)
+      })
+
+    return dates
+  }, [currentMonth, monthAvailability])
+
   return (
     <Container>
       <Header>
@@ -170,10 +212,11 @@ const Dashboard: React.FC = () => {
           <DayPicker
             weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
             fromMonth={new Date()}
-            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
             modifiers={{
               available: { daysOfWeek: [1, 2, 3, 4, 5] },
             }}
+            onMonthChange={handleMonthChange}
             selectedDays={selectedDate}
             onDayClick={handleDateChange}
             months={[
