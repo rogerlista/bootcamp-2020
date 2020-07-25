faf561f46361ddb1db7e4527a88155273fc8ff1f
Rogério <roger.lista@gmail.com>
2020-07-25 07:04:00 -0300

    GoBarber mobile - criada as páginas e rotas de profile criar agendamento e agendamento criado

---
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  return <Container />;
+};
+
+export default AppointmentCreated;
